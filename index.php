2d556c4b3b316a1b9b703ebca6915c38c2f5eaaf
bad0dab <broadnicegamerr@gmail.com>
2025-09-30 07:11:03 +0200

    added page refresh on click

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>
+<button onclick="reloadPage()" style="font-size: 50px; color: black; background-color: white;">Genera nuovi dadi</button>
     <div class="colorInvert">
         <?php
         $dicesToGenerate = 2;
@@ -17,14 +18,18 @@
         {
             return $randomNumber = random_int(1, 6);
         }
-        for ($i=0 ; $i < $dicesToGenerate ; $i++) {
-        $diceContent = file_get_contents('images/dice' . generateNumber() . '.svg');
-        $totalContent = $diceContent . $totalContent;
+        for ($i = 0; $i < $dicesToGenerate; $i++) {
+            $diceContent = file_get_contents('images/dice' . generateNumber() . '.svg');
+            $totalContent = $diceContent . $totalContent;
         }
         echo $totalContent;
         ?>
     </div>
-
+    <script>
+        function reloadPage() {
+            window.location.reload();
+        }
+    </script>
 </body>
 
 </html>
